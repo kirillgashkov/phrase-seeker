38abab64833012307090e4bedccccb72d723dd25
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-02-08 13:52:52 +0500

    Improve downgrader's readability and add documentation

--- idiom_seeker/downgrader.py
+++ idiom_seeker/downgrader.py
@@ -2,23 +2,28 @@
 #
 # Distributed under MIT License. See LICENSE file for details.
 
+import dataclasses
 from typing import List
+
 import spacy
 
 
+@dataclasses.dataclass
 class Word:
-    def __init__(self, text: str, lemma: str, pos: str, dep: str):
-        self.text = text
-        self.lemma = lemma
-        self.pos = pos
-        self.dep = dep
-
-
-    def __repr__(self):
-        return f'<{self.text} {self.lemma} {self.pos} {self.dep}>'
+    """
+    A container for original and downgraded versions of word as well as its
+    lemma and part of speech.
+    """
+    text: str
+    lemma: str
+    pos: str
+    dep: str
 
 
 def downgrade(s: str) -> List[Word]:
+    """
+    Downgrades text into a list of lemmatized words.
+    """
     nlp = spacy.load('en_core_web_sm')
     doc = nlp(s)
     return [Word(tkn.text, tkn.lemma_, tkn.pos_, tkn.dep_) for tkn in doc]
