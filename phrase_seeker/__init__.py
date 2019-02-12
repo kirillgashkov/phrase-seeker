9c2895a9ba9201a76b9dc71d94211c08bcf03d7a
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-02-12 17:49:42 +0500

    Add package __init__ file

---
+++ phrase_seeker/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2019 Kirill Gashkov
+#
+# Distributed under MIT License. See LICENSE file for details.
+
+from phrase_seeker.seeker import seek_phrases_in_text
+
+
+__all__ = {
+    'seek_phrases_in_text',
+}
