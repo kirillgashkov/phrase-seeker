68ec8f3511524b57d242d7fe25f9332598a77b6b
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-02-12 16:32:13 +0500

    Add __contains__() function to phrase

--- phrase_seeker/models/phrase.py
+++ phrase_seeker/models/phrase.py
@@ -20,3 +20,10 @@ class Phrase:
         indefinte_deps = [w.dep for w in self.words if w.lemma == '-INDEF-']
         object.__setattr__(self, 'lemmas', lemmas)
         object.__setattr__(self, 'indefinte_deps', indefinte_deps)
+
+    def __contains__(self, item):
+        if type(item) is not models.Word:
+            return False
+        lemma_check = item.lemma in self.lemmas
+        indefinte_check = item.dep in self.indefinte_deps
+        return lemma_check or indefinte_check
